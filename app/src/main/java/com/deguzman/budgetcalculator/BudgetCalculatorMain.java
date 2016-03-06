fa1bf5730910f8e69712db360abeba81cbc0e0c7
Alejandro Deguzman Jr <adeguzman0923@gmail.com>
2016-03-06 13:32:51 -0800

    Final Project

--- app/src/main/java/com/deguzman/budgetcalculator/BudgetCalculatorMain.java
+++ app/src/main/java/com/deguzman/budgetcalculator/BudgetCalculatorMain.java
@@ -8,13 +8,66 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class BudgetCalculatorMain extends AppCompatActivity {
-//comment
+
+    //declare variables
+    private EditText incomeInput;
+    private EditText expType1;
+    private EditText expType2;
+    private EditText expType3;
+    private EditText expType4;
+    private EditText expType5;
+    private EditText expType6;
+    private EditText expType7;
+    private EditText expType8;
+    private EditText expType9;
+    private EditText expType10;
+    private EditText expAmt1;
+    private EditText expAmt2;
+    private EditText expAmt3;
+    private EditText expAmt4;
+    private EditText expAmt5;
+    private EditText expAmt6;
+    private EditText expAmt7;
+    private EditText expAmt8;
+    private EditText expAmt9;
+    private EditText expAmt10;
+    private Spinner incomeTypeDD;
+    private Spinner numberOfExp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_budget_calculator_main);
+
+        //widget references
+        incomeInput = (EditText) findViewById(R.id.incomeInput);
+        expType1 = (EditText) findViewById(R.id.expType1);
+        expType2 = (EditText) findViewById(R.id.expType2);
+        expType3 = (EditText) findViewById(R.id.expType3);
+        expType4 = (EditText) findViewById(R.id.expType4);
+        expType5 = (EditText) findViewById(R.id.expType5);
+        expType6 = (EditText) findViewById(R.id.expType6);
+        expType7 = (EditText) findViewById(R.id.expType7);
+        expType8 = (EditText) findViewById(R.id.expType8);
+        expType9 = (EditText) findViewById(R.id.expType9);
+        expType10 = (EditText) findViewById(R.id.expType10);
+        expAmt1 = (EditText) findViewById(R.id.expAmt1);
+        expAmt2 = (EditText) findViewById(R.id.expAmt2);
+        expAmt3 = (EditText) findViewById(R.id.expAmt3);
+        expAmt4 = (EditText) findViewById(R.id.expAmt4);
+        expAmt5 = (EditText) findViewById(R.id.expAmt5);
+        expAmt6 = (EditText) findViewById(R.id.expAmt6);
+        expAmt7 = (EditText) findViewById(R.id.expAmt7);
+        expAmt8 = (EditText) findViewById(R.id.expAmt8);
+        expAmt9 = (EditText) findViewById(R.id.expAmt9);
+        expAmt10 = (EditText) findViewById(R.id.expAmt10);
+        incomeTypeDD = (Spinner) findViewById(R.id.incomeTypeDD);
+        numberOfExp = (Spinner) findViewById(R.id.numberOfExp);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,27 +79,5 @@ public class BudgetCalculatorMain extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_budget_calculator_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
